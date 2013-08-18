edd2ea7d0bd8c23c30a3488f9d80f22747fccceb
Kyle Graehl <kgraehl@spotify.com>
2013-08-17 18:01:18 -0700

    package

--- js/popup.js
+++ js/popup.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Permissions must be requested from inside a user gesture, like a button's
 	// click handler.
 	chrome.permissions.request({
-	    permissions: ["tabs"],
+	    permissions: ["tabs","notifications","contextMenus"],
 	    origins: ["<all_urls>"]
 	}, function(granted) {
 	    // The callback argument will be true if the user granted the permissions.
